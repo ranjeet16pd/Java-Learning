33fd5ed14166ad7a38fefa293eb8e5a5a19719b8
Ranjeet Prasad <ranjeet.prasad@paytm.com>
2025-02-08 23:25:38 +0530

    learned basic of array

--- Array.java
+++ Array.java
@@ -6,12 +6,46 @@ public class Array {
    
     public static void main(String[] args){
     
+
+        System.out.println("way 1");
         int arr[]={1,24,5,6,7};
         int n=arr.length;
 
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
+
+      System.out.println();
+      System.out.println("way 2");
+
+      // this will allocate the memory and with 5 elemnet and its default value is 0
+      int[] arr2=new int[5];
+
+      for(int i=0;i<5;i++){
+        System.out.print(arr2[i]+" ");
+      }
+
+
+
+
+      // multi - dimensiom array
+      System.out.println();
+      System.out.println("multi dimension");
+
+      int[][] multiArray = new int[][]{{1,2,4},{2,4,6}};
+
+      int n1=multiArray.length;
+      for(int i=0;i<n1;i++){
+        //for each loop
+        System.out.println();
+        for(int it : multiArray[i]){
+          System.out.print(it+" ");
+        }
+    }
+      
+
+      
+
 
 
 
